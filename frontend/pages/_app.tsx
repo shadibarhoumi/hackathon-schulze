133c6eb84206e4f7648de5425693acc7b33e53ef
Shadi Barhoumi <shadibarhoumi@gmail.com>
2023-03-24 10:08:47 -0600

    Add op goerli support

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,21 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-import { Config, DAppProvider, Goerli, Mainnet } from '@usedapp/core'
+import {
+  Config,
+  DAppProvider,
+  Goerli,
+  OptimismGoerli,
+  Mainnet,
+} from '@usedapp/core'
 import { providers } from 'ethers'
 
 const useDappConfig: Config = {
   readOnlyChainId: Mainnet.chainId,
   readOnlyUrls: {
+    [OptimismGoerli.chainId]: new providers.InfuraProvider(
+      'optimism-goerli',
+      process.env.NEXT_PUBLIC_INFURA_API_KEY!,
+    ),
     [Goerli.chainId]: new providers.InfuraProvider(
       'goerli',
       process.env.NEXT_PUBLIC_INFURA_API_KEY!,
